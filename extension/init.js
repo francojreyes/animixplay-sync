4a08785fd282c22871455d2ee30ca35d9b1086d7
Franco Reyes <franco.javier.reyes@gmail.com>
2022-07-15 02:15:49 +1000

    Added image to button

--- extension/init.js
+++ extension/init.js
@@ -2,14 +2,14 @@
 // Possibly add a popup for sessions
 ready('.plyr__controls', function (controls) {  
     const btn = document.createElement("button");
-    btn.classList.add("plyr__controls__item", "plyr__control", "plyr__controls__sync");
+    btn.classList.add("plyr__controls__sync", "plyr__control");
     btn.addEventListener('click', () => {
         console.log('button pressed');
         sync();
     });
 
     const img = document.createElement("img");
-    img.src = chrome.runtime.getURL("icons/24.png");
+    img.src = chrome.runtime.getURL("icons/18.png");
     btn.appendChild(img);
 
     controls.prepend(btn);
