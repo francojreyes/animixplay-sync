ca308292db1b7259109923ba0f2c07fd0958d6be
Franco Reyes <franco.javier.reyes@gmail.com>
2022-07-15 14:14:30 +1000

    Formatted button

--- extension/init.js
+++ extension/init.js
@@ -1,16 +1,28 @@
 // Add a new sync button next to play
 // Possibly add a popup for sessions
 ready('.plyr__controls', function (controls) {  
+    const div = document.createElement("div");
+    div.classList.add("plyr__controls__item", "plyr__menu");
+
     const btn = document.createElement("button");
-    btn.classList.add("plyr__controls__sync", "plyr__control");
+    btn.classList.add("plyr__sync", "plyr__control");
+    btn.type = "button";
     btn.addEventListener('click', () => {
         console.log('button pressed');
         sync();
     });
 
     const img = document.createElement("img");
-    img.src = chrome.runtime.getURL("icons/18.png");
-    btn.appendChild(img);
+    img.src = chrome.runtime.getURL("icons/unpressed.svg");
+    img.onerror = chrome.runtime.getURL("icons/unpressed.png");
+    img.classList.add("plyr__sync__img");
 
-    controls.prepend(btn);
+    const span = document.createElement("span");
+    span.classList.add("plyr__tooltip");
+    span.textContent = "Sync";
+
+    btn.appendChild(img);
+    btn.appendChild(span);
+    div.appendChild(btn);
+    controls.prepend(div);
 });
